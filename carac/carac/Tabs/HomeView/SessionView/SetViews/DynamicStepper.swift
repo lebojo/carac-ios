d14848c554bb3690293e34ca3fc4acd8a0007116
LeBojo <contact@jordan-chap.ch>
2025-12-01 23:03:08 +0100

    feat: Dynamic stepper looks good on iOS 26

--- carac/carac/Tabs/HomeView/SessionView/SetViews/DynamicStepper.swift
+++ carac/carac/Tabs/HomeView/SessionView/SetViews/DynamicStepper.swift
@@ -18,12 +18,26 @@ struct DynamicStepper: View {
     let isFinished: Bool
 
     var body: some View {
-        HStack {
-            Label(text, systemImage: systemImage)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            if !isFinished {
-                Stepper("", value: value, in: 0 ... 200, step: step)
+        if #available(iOS 26.0, *) {
+            if isFinished {
+                Label(text, systemImage: systemImage)
+            } else {
+                Stepper(value: value, in: 0 ... 200, step: step) {
+                    Label(text, systemImage: systemImage)
+                }
             }
+        } else {
+            ZStack {
+                Stepper("", value: value, in: 0 ... 200, step: step)
+                    .opacity(isFinished ? 0 : 1)
+                    .labelsHidden()
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+
+                Label(text, systemImage: systemImage)
+                    .lineLimit(1, reservesSpace: true)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
         }
     }
 }
