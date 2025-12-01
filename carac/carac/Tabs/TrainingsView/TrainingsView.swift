af7cfd400a94da62dd6ab57d80bf57994b18e68d
LeBojo <contact@jordan-chap.ch>
2025-12-01 23:38:33 +0100

    feat: Training display exercise too

--- carac/carac/Tabs/TrainingsView/TrainingsView.swift
+++ carac/carac/Tabs/TrainingsView/TrainingsView.swift
@@ -13,29 +13,50 @@ struct TrainingsView: View {
     @EnvironmentObject var mainViewState: MainViewState
 
     @Query private var trainings: [Training]
-    
+    @Query private var exercises: [Exercise]
+
     private var singleTrainings: [Training] {
         trainings.filter { $0.sessions.isEmpty }
+    }
+
+    private var singleExercises: [Exercise] {
+        exercises.filter(\.sets.isEmpty)
     }
 
     var body: some View {
         NavigationStack {
             List {
-                ForEach(singleTrainings, id: \.persistentModelID) { training in
-                    Button {
-                        mainViewState.selectedTraining = training
-                    } label: {
-                        HStack {
-                            Text(training.title)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Image(systemName: "chevron.right")
+                Section("Trainings") {
+                    ForEach(singleTrainings, id: \.persistentModelID) { training in
+                        Button {
+                            mainViewState.selectedTraining = training
+                        } label: {
+                            HStack {
+                                Text(training.title)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Image(systemName: "chevron.right")
+                            }
+                        }
+                    }
+                    .onDelete { indexSet in
+                        for index in indexSet {
+                            let trainingToDelete = singleTrainings[index]
+                            modelContext.delete(trainingToDelete)
                         }
                     }
                 }
-                .onDelete { indexSet in
-                    for index in indexSet {
-                        let trainingToDelete = singleTrainings[index]
-                        modelContext.delete(trainingToDelete)
+
+                Section("Exercises") {
+                    ForEach(singleExercises, id: \.persistentModelID) { exercise in
+                        Button {
+                            mainViewState.selectedExercise = exercise
+                        } label: {
+                            HStack {
+                                Text(exercise.name)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Image(systemName: "chevron.right")
+                            }
+                        }
                     }
                 }
             }
