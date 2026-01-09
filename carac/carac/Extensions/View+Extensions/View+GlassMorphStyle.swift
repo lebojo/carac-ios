04f2488a9a96fecbabe05da0844a720cfe081bd8
LeBojo <contact@jordan-chap.ch>
2026-01-09 13:01:54 +0100

    feat: Add Glassmorphstyle

---
+++ carac/carac/Extensions/View+Extensions/View+GlassMorphStyle.swift
@@ -0,0 +1,46 @@
+//
+//  View+GlassMorphStyle.swift
+//  SudokuInfinite
+//
+//  Created by Jordan on 24.05.902025.
+//
+
+import SwiftUI
+
+extension View {
+    func glassEffectStyle(_ glassEffect: GlassEffect.GlassType = .regular, cornerRadius: CGFloat = 20) -> some View {
+        modifier(GlassEffect(glassEffect: glassEffect, cornerRadius: cornerRadius))
+    }
+}
+
+struct GlassEffect: ViewModifier {
+    let glassEffect: GlassType
+    let cornerRadius: CGFloat
+
+    func body(content: Content) -> some View {
+        if #available(iOS 26.0, *) {
+            content
+                .glassEffect(glassEffect.nativeGlass, in: .rect(cornerRadius: cornerRadius))
+        } else {
+            content
+                .background(.thinMaterial)
+                .cornerRadius(cornerRadius)
+                .shadow(radius: 3)
+        }
+    }
+
+    enum GlassType {
+        case clear
+        case regular
+
+        @available(iOS 26.0, *)
+        var nativeGlass: Glass {
+            switch self {
+            case .clear:
+                .clear
+            case .regular:
+                .regular
+            }
+        }
+    }
+}
